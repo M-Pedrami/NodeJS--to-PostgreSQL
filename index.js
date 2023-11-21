49322dddbfd7ac70c00b9235da24d560b4c61ac3
Maziar Pedrami <awildroot@gmail.com>
2023-11-21 12:09:25 +0100

    added pool data in env

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { Pool } = require("pg");
+
+//Connecting to the DB
+
+const pool = new Pool({
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: 5432,
+});
 
 //Server: users: getAll
 
-app.get(('/'), (req, res)=>{
-
-})
+app.get("/", (req, res) => {});
 
 //users/:id
 
-app.get(('/:id'), (req, res)=>{
-
-})
+app.get("/:id", (req, res) => {});
 
 //users POST create a new user
 
-app.post(('/'), (req, res)=>{
-
-})
+app.post("/", (req, res) => {});
 
 // users PUT  update a user
 
-app.put(('/:id'), (req, res)=>{
-  
-})
+app.put("/:id", (req, res) => {});
 
 // users DELETE  delete a user
 
-app.delete(('/:id'), (req, res)=>{
-
-})
+app.delete("/:id", (req, res) => {});
 
 app.listen(PORT, console.log(`server is running on port : ${PORT}`));
